a06d34e5d074d605b7b55a17fe37bb2945c9ab65
Bhanu <chandubhanuprakash123@gmail.com>
2024-08-02 20:03:15 +0530

    First Commit

---
+++ src/main/java/NewSeleniumPractice/magento/App.java
@@ -0,0 +1,13 @@
+package NewSeleniumPractice.magento;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
